4ff024448c6bd400fca9b6b1232af3cd321ae0ff
Shikhar Tuli <shikhartuli98@gmail.com>
2020-12-25 23:06:25 +0530

    Update gpu_env_setup.sh

--- vision_transformer/gpu_env_setup.sh
+++ vision_transformer/gpu_env_setup.sh
@@ -4,12 +4,12 @@
 # Author : Shikhar Tuli
 
 module load anaconda3
-conda create --name cnn_txf_bias python=3.6 anaconda
+conda create --name ctb_test python=3.6.9 anaconda
 
-conda activate cnn_txf_bias
+conda activate ctb_test
 
 pip install -qr ./vit_jax/requirements_new.txt
 pip install tfa-nightly
 pip install tensorflow_io
-pip install --upgrade jax jaxlib==0.1.57+cuda110 -f https://storage.googleapis.com/jax-releases/jax_releases.html
+pip install --upgrade jax jaxlib==0.1.57+cuda101 -f https://storage.googleapis.com/jax-releases/jax_releases.html
 pip install opencv-python
